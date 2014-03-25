dbf8106014304953c3c13dc18f5dfe18274d7633
Eason Lin <easonylin@gmail.com>
2014-03-24 17:31:06 -0700

    Add basic scene pages

--- app.rb
+++ app.rb
@@ -5,20 +5,35 @@ require ::File.expand_path('../config/environment',  __FILE__)
 
 set :app_file, __FILE__
 
+get '/' do
+  first_scene = Scene.first_scene
+
+  if first_scene
+    redirect to("/scenes/#{first_scene.id}")
+  else
+    redirect to('/scenes')
+  end
+end
+
+get '/scenes/:id' do
+  scene = Scene.where(:id => params[:id]).first
+  erb :scene, :locals => {:scene => scene}
+end
+
+get '/scenes' do
+  scenes = Scene.order(:created_at)
+  erb :scenes, :locals => {:scenes => scenes}
+end
+
 post '/scenes' do
   # create a new scene
   Scene.create(:parent_id => params[:parent_id], :title => params[:title], :body => params[:body])
   # How? Did this work?
-  redirect to('/')
-end
-
-get '/' do
-  scenes = Scene.order(:created_at)
-  erb :index, :locals => {:scenes => scenes}
+  redirect to('/scenes')
 end
 
 post "/scenes/:id/delete" do
   scene = Scene.where(:id => params[:id])
   scene.delete
-  redirect to('/')
+  redirect to('/scenes')
 end
--- models/scene.rb
+++ models/scene.rb
@@ -1,3 +1,7 @@
 class Scene < Sequel::Model(:scenes)
 	one_to_many :next_scenes, :key => :parent_id, :class => self
+
+  def self.first_scene
+    where('parent_id IS NULL').first
+  end
 end
